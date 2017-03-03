12de94700c203741d1f11477976c45bc62fb90ff
Matthew Howard <matthow73@gmail.com>
2017-03-02 16:29:33 -0800

    Create the skeleton, add data

---
+++ ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+state.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_State.csv")
+hospital.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_Hospital.csv")
+measures <- unique(state.data$Measure.Name)
+
+ui <- fluidPage(
+  titlePanel("Outpatient Efficiency for Selected Measures"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("measure", "Choose a measure:", 
+                  choices = measures),
+      width = 5
+    ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+        tabPanel("Results"),
+        tabPanel("Map"),
+        tabPanel("Plot")
+      )
+    )
+  )
+)
