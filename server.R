9356a028aa9d0eaf904988c9429bec1ec2cc7354
Akshat Aggarwal <akshat010598@gmail.com>
2017-03-04 14:17:34 -0800

    Complete Results Tab

--- server.R
+++ server.R
@@ -6,6 +6,20 @@ state.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_State.csv")
 hospital.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_Hospital.csv")
 measures <- unique(state.data$Measure.Name)
 
-function(input, output) {
+server <- function(input, output) {
+  filtered.data <- reactive({
+    state <- input$state
+    measure <- input$measure
+    best.hospital <- filter(hospital.data, State == state) %>% 
+                     filter(Measure.Name == measure) %>% 
+                     arrange(desc(Score))
+    best.hospital <- best.hospital[!(best.hospital$Score == "Not Available"), ]
+    best.hospital <- best.hospital[c(1:5), c("Hospital.Name", "City", "Address", "ZIP.Code", "Phone.Number")]
+    return(best.hospital)
+  })
+  
+  output$best.hospitals <- renderDataTable({
+    return(filtered.data())
+  })
   
 }
--- ui.R
+++ ui.R
@@ -5,6 +5,8 @@ library(ggplot2)
 state.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_State.csv")
 hospital.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_Hospital.csv")
 measures <- unique(state.data$Measure.Name)
+states <- unique(state.data$State)
+
 
 ui <- fluidPage(
   titlePanel("Outpatient Efficiency for Selected Measures"),
@@ -16,7 +18,9 @@ ui <- fluidPage(
     ),
     mainPanel(
       tabsetPanel(type = "tabs",
-        tabPanel("Results"),
+        tabPanel("Results", 
+                 selectInput('state', label = "Select state", choices = states),
+                 dataTableOutput('best.hospitals') ) ,
         tabPanel("Map"),
         tabPanel("Plot")
       )
