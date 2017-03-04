034b3a2849e45f0dec86fba73454068e70bf4ee6
josephktsai <josephkt@uw.edu>
2017-03-03 17:02:44 -0800

    Change plot to plotly

--- server.R
+++ server.R
@@ -10,26 +10,37 @@ state.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_State.csv")
 hospital.data <- read.csv("data/Outpatient_Imaging_Efficiency_-_Hospital.csv")
 measures <- unique(state.data$Measure.Name)
 us.map <- map_data("state")
+radiologist.data <- read.csv("data/Physician_Compare_National_Downloadable_File.csv", stringsAsFactors = FALSE)
+
 interactive.graph.data <- reactiveValues()
 interactive.graph.data$top5 <- ""
+
 function(input, output) {
   
   # Creating the USA map by state
   output$map <- renderPlotly({
-    states <- us.map %>%
-              group_by(region)
-    
+    # Finding the number of radiologists by state
+    num.radiologists.by.state <- radiologist.data %>%
+                                 group_by(State) %>%
+                                 summarise(n = n()) %>%
+                                 select(n)
+
+    # Creating the output for when somebody hovers over a state
+    num.radiologists.by.state$hover <- with(num.radiologists.by.state, 
+                                            paste("Number of radiologists:", n))
     # Specifying the map scope
     map.specifications <- list(
-      scope = 'usa'
+      scope = 'usa',
+      projection = list(type = "albers usa")
     )
     
-    # Creating the plot with plotly so that it will be interactive between the state borders
-    plot_ly(z = us.map$region, text = state.name, locations = state.abb,
-            type = 'choropleth', locationmode = 'USA-states') %>%
+    radiologists.and.states <- plot_geo(num.radiologists.by.state, locationmode = "USA-states") %>%
+      add_trace(z = ~n, text = ~hover, color = ~n, colors = "Purples"
+      ) %>%
       
       # Choosing the layout based off of our map specifications 
-      layout(geo = map.specifications)
+      layout(title = "Number of radiologists by state",
+             geo = map.specifications)
   })
 
 
